9da9b074188301d2811e3eaa7ffa1ef870efe28f
rodrigovallades <rodrigo.vallades@gmail.com>
2018-02-04 17:35:16 -0200

    Styling and adding functions to the form

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,21 +1,70 @@
-var signinUrl = 'https://private-47ed5-interviewapitest.apiary-mock.com/signin'
+(function() {
+  var signinUrl = 'https://private-47ed5-interviewapitest.apiary-mock.com/signin',
+  loginForm = '#login';
 
-function post(url, data, cb) {
-  var xhr = new XMLHttpRequest()
-  console.log(xhr)
-  xhr.onload = function() {
-    cb(JSON.parse(this.responseText))
+  function post(url, data, cb) {
+    var xhr = new XMLHttpRequest();
+    console.log(xhr);
+    xhr.onload = function() {
+      cb(JSON.parse(this.responseText));
+    }
+    xhr.open('POST', url);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
   }
-  xhr.open('POST', url)
-  xhr.setRequestHeader("Content-Type", "application/json")
-  xhr.send(JSON.stringify(data))
-}
 
-window.submitForm = function() {
-  post(signinUrl, {
-    username: document.querySelector('#username').value,
-    password: document.querySelector('#password').value
-  }, function(res) {
-    console.log(res)
-  })
-}
+  function blockUI(status, el) {
+    var spinnerClass = 'block-spinner',
+    blockUIClass = 'block-ui',
+    loader = document.createElement('div'),
+    blockEl = document.querySelector(el);
+
+    loader.classList.add(spinnerClass);
+    blockEl.classList.add(blockUIClass);
+
+    if (status) {  //block
+      blockEl.appendChild(loader);
+    } else { // unblock
+      var blockedClass = '.' + blockUIClass + ' .' +  spinnerClass,
+      blocked = document.querySelector(blockedClass);
+
+      if (blocked) {
+        blocked.remove()
+      }
+    }
+  }
+
+  $.validate({
+    form : loginForm,
+    modules: 'security',
+    onModulesLoaded : function() {
+      var optionalConfig = {
+        fontSize: '14px',
+        padding: '0',
+        bad : 'Very weak',
+        weak : 'Weak',
+        good : 'Good',
+        strong : 'Strong'
+      };
+      $('input[type="password"]').displayPasswordStrength(optionalConfig);
+    },
+    onSuccess: function() {
+      blockUI(true, loginForm);
+      post(signinUrl, {
+        username: document.querySelector('#username').value,
+        password: document.querySelector('#password').value
+      }, function(res) {
+        blockUI(false, loginForm);
+        if (res) {
+          console.log(res);
+          if (res.error.msg) {
+            alert(res.error.msg);
+          }
+        }
+
+      })
+      return false;
+    }
+  });
+
+})();
