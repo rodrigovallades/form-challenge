0aa5734cf6b00f20ba99bae09f5b68d3aff619b8
rodrigovallades <rodrigo.vallades@gmail.com>
2018-02-04 11:24:56 -0200

    Organize gulp tasks

--- gulpTasks/app.js
+++ gulpTasks/app.js
@@ -8,32 +8,32 @@ const sass = require('gulp-sass')
 gulp.task('app', ['app.html', 'app.css', 'app.scss', 'app.js', 'app.assets'])
 
 gulp.task('app.html', () => {
-    return gulp.src('app/**/*.html')
-        //.pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('public'))
+  return gulp.src('app/**/*.html')
+    //.pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('public'))
 })
 
 gulp.task('app.css', () => {
-    return gulp.src('app/**/*.css')
-        //.pipe(uglifycss({ "uglyComments": true }))
-        .pipe(concat('index.css'))
-        .pipe(gulp.dest('public/assets/css'))
+  return gulp.src('app/**/*.css')
+    //.pipe(uglifycss({ "uglyComments": true }))
+    .pipe(concat('index.css'))
+    .pipe(gulp.dest('public/assets/css'))
 })
 
 gulp.task('app.scss', function() {
-    gulp.src('app/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('public/assets/css'));
+  return gulp.src('app/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/assets/css'));
 })
 
 gulp.task('app.js', () => {
-    return gulp.src('app/**/*.js')
-        //.pipe(uglify({ mangle: false }))
-        .pipe(concat('index.js'))
-        .pipe(gulp.dest('public/assets/scripts'))
+  return gulp.src('app/**/*.js')
+    //.pipe(uglify({ mangle: false }))
+    .pipe(concat('index.js'))
+    .pipe(gulp.dest('public/assets/scripts'))
 })
 
 gulp.task('app.assets', () => {
-    return gulp.src('app/assets/**/*.*')
-        .pipe(gulp.dest('public/assets'))
+  return gulp.src('app/assets/**/*.*')
+    .pipe(gulp.dest('public/assets'))
 })
--- gulpTasks/server.js
+++ gulpTasks/server.js
@@ -3,18 +3,18 @@ const watch = require('gulp-watch')
 const webserver = require('gulp-webserver')
 
 gulp.task('watch', () => {
-    watch('app/**/*.html', () => gulp.start('app.html'))
-    watch('app/**/*.css', () => gulp.start('app.css'))
-    watch('app/**/*.scss', () => gulp.start('app.scss'))
-    watch('app/**/*.js', () => gulp.start('app.js'))
-    watch('app/assets/*.*', () => gulp.start('app.assets'))
+  watch('app/**/*.html', () => gulp.start('app.html'))
+  watch('app/**/*.css', () => gulp.start('app.css'))
+  watch('app/**/*.scss', () => gulp.start('app.scss'))
+  watch('app/**/*.js', () => gulp.start('app.js'))
+  watch('app/assets/*.*', () => gulp.start('app.assets'))
 })
 
 gulp.task('server', ['watch'], () => {
-    return gulp.src('public').pipe(webserver({
-        livereload: true,
-        port: 3000,
-        fallback: 'index.html',
-        open: true
-    }))
+  return gulp.src('public').pipe(webserver({
+    livereload: true,
+    port: 3000,
+    fallback: 'index.html',
+    open: true
+  }))
 })
